a0357fa05a5911e8adc4e34f2d07c94bbb0f2d87
greetingsForAlek <alexpwoods@outlook.com>
2025-06-28 22:07:33 +0100

    fixing bugs lol

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,8 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
 	pingTimeout: 60000,
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "*",
+		methods: ["GET", "POST"],
 	},
 });
 
